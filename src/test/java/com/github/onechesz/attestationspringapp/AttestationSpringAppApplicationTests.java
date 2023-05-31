bbedea36236b1bf69cd357f6f66614baba2d2862
an4yta1 <134634477+an4yta1@users.noreply.github.com>
2023-05-31 12:31:59 +0300

    Update AttestationSpringAppApplicationTests.java

--- src/test/java/com/github/onechesz/attestationspringapp/AttestationSpringAppApplicationTests.java
+++ src/test/java/com/github/onechesz/attestationspringapp/AttestationSpringAppApplicationTests.java
@@ -1,13 +1,1 @@
-package com.github.onechesz.attestationspringapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AttestationSpringAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
